c6b4f9aa69d24af0b90501dfc55d0175e14e303b
xrkffgg <xrkffgg@vip.qq.com>
2019-12-31 11:27:45 +0800

    add uuid

--- JS/003.Utils/main.js
+++ JS/003.Utils/main.js
@@ -7,6 +7,46 @@ export function getUUID () {
   })
 }
 
+generateUUID() { 
+  var d = new Date().getTime()
+  if (window.performance && typeof window.performance.now === "function") { 
+      d += performance.now()
+  }
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) { 
+      var r = (d + Math.random() * 16) % 16 | 0; d = Math.floor(d / 16) 
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+  })
+  return uuid
+}
+
+guid() { 
+  function S4() { 
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  } 
+  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4())
+}
+
+function uuid2(len, radix) {
+  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+  var uuid = [],
+      i;
+  radix = radix || chars.length;
+
+  if (len) {
+      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+  } else {
+      var r;
+      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+      uuid[14] = '4';
+      for (i = 0; i < 36; i++) {
+          if (!uuid[i]) {
+              r = 0 | Math.random() * 16;
+              uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+          }
+      }
+  }
+  return uuid.join('');
+}
 /**
  * 邮箱
  * @param {*} s
