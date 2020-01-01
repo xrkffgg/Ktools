39e9f414c4b3f86e2bb42b857b8bb650033e17c8
xrkffgg <xrkffgg@vip.qq.com>
2020-01-01 16:47:38 +0800

    fix

--- JS/003.Utils/main.js
+++ JS/003.Utils/main.js
@@ -1,49 +1,49 @@
 /**
  * 获取uuid
  */
-export function getUUID () {
+export function getUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     return (c === 'x' ? (Math.random() * 16 | 0) : ('r&0x3' | '0x8')).toString(16)
   })
 }
 
-generateUUID() { 
+generateUUID() {
   var d = new Date().getTime()
-  if (window.performance && typeof window.performance.now === "function") { 
-      d += performance.now()
+  if (window.performance && typeof window.performance.now === "function") {
+    d += performance.now()
   }
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) { 
-      var r = (d + Math.random() * 16) % 16 | 0; d = Math.floor(d / 16) 
-      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0; d = Math.floor(d / 16)
+    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
   })
   return uuid
 }
 
-guid() { 
-  function S4() { 
-      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-  } 
+guid() {
+  function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
   return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4())
 }
 
 function uuid2(len, radix) {
   var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
   var uuid = [],
-      i;
+    i;
   radix = radix || chars.length;
 
   if (len) {
-      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
   } else {
-      var r;
-      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-      uuid[14] = '4';
-      for (i = 0; i < 36; i++) {
-          if (!uuid[i]) {
-              r = 0 | Math.random() * 16;
-              uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
-          }
+    var r;
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    uuid[14] = '4';
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | Math.random() * 16;
+        uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
       }
+    }
   }
   return uuid.join('');
 }
@@ -51,7 +51,7 @@ function uuid2(len, radix) {
  * 邮箱
  * @param {*} s
  */
-export function isEmail (s) {
+export function isEmail(s) {
   return /^([a-zA-Z0-9._-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(s)
 }
 
@@ -59,7 +59,7 @@ export function isEmail (s) {
  * 手机号码
  * @param {*} s
  */
-export function isMobile (s) {
+export function isMobile(s) {
   return /^1[0-9]{10}$/.test(s)
 }
 
@@ -67,7 +67,7 @@ export function isMobile (s) {
  * 电话号码
  * @param {*} s
  */
-export function isPhone (s) {
+export function isPhone(s) {
   return /^([0-9]{3,4}-)?[0-9]{7,8}$/.test(s)
 }
 
@@ -75,6 +75,6 @@ export function isPhone (s) {
  * URL地址
  * @param {*} s
  */
-export function isURL (s) {
+export function isURL(s) {
   return /^http[s]?:\/\/.*/.test(s)
 }
