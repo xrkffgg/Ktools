9089c44558f7fbaca49ffb499c26d5addde643a6
xrkffgg <xrkffgg@vip.qq.com>
2020-09-29 17:45:40 +0800

    add downloadIamge

--- JS/005.Canvas/main.js
+++ JS/005.Canvas/main.js
@@ -23,6 +23,7 @@ export default {
       ctx.putImageData(imageData, 0,0)
     },
 
+    // 反向
     do5(){
       var cav = document.getElementById('myca')
       var ctx = cav.getContext('2d')
@@ -44,6 +45,26 @@ export default {
 
     getImageData(cav,ctx){
       return ctx.getImageData(0,0,cav.width,cav.height)
+    },
+
+    downloadIamge (imgsrc, name) { // 下载图片地址和图片名
+      let image = new Image()
+      // 解决跨域 Canvas 污染问题
+      image.setAttribute('crossOrigin', 'anonymous')
+      image.onload = function () {
+        let canvas = document.createElement('canvas')
+        canvas.width = image.width
+        canvas.height = image.height
+        let context = canvas.getContext('2d')
+        context.drawImage(image, 0, 0, image.width, image.height)
+        let url = canvas.toDataURL('image/jpg') // 得到图片的base64编码数据
+        let a = document.createElement('a') // 生成一个a元素
+        let event = new MouseEvent('click') // 创建一个单击事件
+        a.download = name || 'photo' // 设置图片名称
+        a.href = url // 将生成的URL设置为a.href属性
+        a.dispatchEvent(event) // 触发a的单击事件
+      }
+      image.src = imgsrc + '?time=' + new Date().getTime()
     },
   }
 }
